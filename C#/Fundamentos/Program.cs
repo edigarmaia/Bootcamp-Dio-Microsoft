f7927acbcb5ab474361a770cc1af88c44294f723
Edigar Maia <edigarmaia@gmail.com>
2023-04-09 23:20:47 -0300

    trabalhando com data

--- C#/Fundamentos/Program.cs
+++ C#/Fundamentos/Program.cs
@@ -1,16 +1,24 @@
 ﻿using Fundamentos.Models;
 
-string apresentacao = "Olá, seja bem vindo";
-int quantidade = 1;
-double altura = 1.80;
-decimal preco = 1.80M;
-bool condicao = true;
+DateTime dataAtual = DateTime.Now;
+DateTime dataAtualMais5 = DateTime.Now.AddDays(5);
+//Console.WriteLine(dataAtual);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+Console.WriteLine(dataAtualMais5);
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("\nValor da quantidade " +  quantidade);
-Console.WriteLine("\nValor da altura " +  altura.ToString("0.00"));
-Console.WriteLine("\nValor da preço " +  preco);
-Console.WriteLine("\nValor da condição " +  condicao);
+
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("\nValor da quantidade " +  quantidade);
+// Console.WriteLine("\nValor da altura " +  altura.ToString("0.00"));
+// Console.WriteLine("\nValor da preço " +  preco);
+// Console.WriteLine("\nValor da condição " +  condicao);
 
 
 /*
