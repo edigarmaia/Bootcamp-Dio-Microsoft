c7a294a5688e8acc1606b94ecd523c1956e3aa5e
Edigar Maia <edigarmaia@gmail.com>
2023-04-19 23:47:40 -0300

    Aula propriedades

--- C#/Anotacoes.md
+++ C#/Anotacoes.md
@@ -121,4 +121,3 @@ for(int i = 0; i < arrayInteiros.Length; i++){
 
 REDIMENSIONANDO UM ARRAY
 
-# LISTAS
---
+++ C#/ExemploExplorando/Program.cs
@@ -0,0 +1,7 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Edigar";
+p1.Idade = 40;
+
+p1.Apresentar();
