5e745e857f8b27fbbf67e01f1ed7985f8693f965
Edigar Maia <edigarmaia@gmail.com>
2023-04-23 01:37:11 -0300

    Finalizado módulo - manipulando valores com C#

--- C#/ExemploExplorando/Program.cs
+++ C#/ExemploExplorando/Program.cs
@@ -1,23 +1,74 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;//Alterando a localização do código
 
-Pessoa p1 = new Pessoa(nome: "Edigar", sobrenome: "Maia dos Santos");
-// p1.Nome = "Edigar";
-// p1.SobreNome = "Maia";
+string dataString = "2023-04-17 18:00";
+bool sucesso = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", 
+                CultureInfo.InvariantCulture, 
+                DateTimeStyles.None, 
+                out DateTime data);
 
-Pessoa p2 = new Pessoa(nome: "Paula", sobrenome: "Souza");
-// p2.Nome = "Paula";
-// p2.SobreNome = "Souza";
+if(sucesso){
+    Console.WriteLine($"Conversão com sucesso! Data: {data}");
+}else{
+    Console.WriteLine($"{dataString} não é uma data válida!");
+}
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// DateTime data = DateTime.Parse(dataString);
+// Console.WriteLine(data);
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+// DateTime data = DateTime.Now;//data e hora atual do computador
+
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data);
+// Console.WriteLine(data.ToShortTimeString());
+// Console.WriteLine(data.ToString("dd/MM/yyyy hh:mm"));
+// Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
 
 
 
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1282.40M;
+
+// // Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+// Console.WriteLine(valorMonetario.ToString("C1"));//uma casa decimal
+// Console.WriteLine(valorMonetario.ToString("C2"));//duas casa decimal
+// Console.WriteLine(valorMonetario.ToString("N2"));//duas casa decimal, sem o R$
+
+// double porcentangem = .3421;
+// Console.WriteLine(porcentangem.ToString("P"));
+
+// int numero = 12345678;
+// Console.WriteLine(numero.ToString("##-##-####"));
+
+
+
+// int numero1 = 10;
+// int numero2 = 20;
+
+// int resultado = numero1 + numero2;
+
+// Console.WriteLine(resultado);
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "Edigar", sobrenome: "Maia dos Santos");
+// // p1.Nome = "Edigar";
+// // p1.SobreNome = "Maia";
+
+// Pessoa p2 = new Pessoa(nome: "Paula", sobrenome: "Souza");
+// // p2.Nome = "Paula";
+// // p2.SobreNome = "Souza";
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
 
--- C#/ExemploExplorando/anotacoes.md
+++ C#/ExemploExplorando/anotacoes.md
@@ -27,3 +27,35 @@ string texto = "Nº " + (count+1) + " - " + Alunos[count].NomeCompleto;
 
 # INTERPOLAÇÃO DE STRINGS
 string texto = $"Nº {count+1} - {Alunos[count].NomeCompleto}";
+
+# FORMATANDO VALORES MONETÁRIOS
+decimal valorMonetario = 82.40M;
+Console.WriteLine($"{valorMonetario:c}"); -> retorna R$ 82,40
+:C -> indica a moeda corrente configurada no computador
+
+# Alterando a localização do código
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");//Para dólar
+
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); //para dólar
+
+Console.WriteLine(valorMonetario.ToString("C1"));//uma casa decimal
+Console.WriteLine(valorMonetario.ToString("C2"));//duas casa decimal
+Console.WriteLine(valorMonetario.ToString("N2"));//duas casa decimal, sem o R$
+
+# PORCENTAGEM
+double porcentangem = .3421;
+Console.WriteLine(porcentangem.ToString("P")); -> retorna 34,21%
+
+# NUMEROS
+int numero = 12345678;
+Console.WriteLine(numero.ToString("##-##-####")); -> retorna 12-34-5678
+
+# DATAS
+DateTime data = DateTime.Now;//data e hora atual do computador
+
+Console.WriteLine(data.ToShortDateString());
+Console.WriteLine(data);
+Console.WriteLine(data.ToString("dd/MM/yyyy hh:mm"));
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
